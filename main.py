768e79133c5904166938c359c7c0cce4de128b50
gitmonkus <ironmonkus@gmail.com>
2025-06-24 21:34:12 -0400

    main.py - added arguments

--- main.py
+++ main.py
@@ -3,9 +3,14 @@
 from stats import get_num_words
 from stats import get_num_char
 from stats import get_num_sort
+import sys
 
 def main():
-    book_path = "books/frankenstein.txt"
+    if not len(sys.argv) > 1:
+        print("Usage: python3 main.py <path_to_book>")
+        sys.exit(1)
+
+    book_path = sys.argv[1]
     text = get_book_text(book_path)
     num_words = get_num_words(text)
     num_char = get_num_char(text)
